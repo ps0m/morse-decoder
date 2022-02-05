868e05343043e49d8c5bbfada3490e1f4f573113
ps0m <ps0m@tut.by>
2022-02-06 01:27:57 +0300

    feat: create Object MORSE_TABLE_ONE_ZERO

--- src/index.js
+++ src/index.js
@@ -38,9 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = '';
+    const MORSE_TABLE_ONE_ZERO = {};
+    for (key in MORSE_TABLE) {
+        let mt='';
+        [key].splice("").map(function (x){
+            for (point of x) {
+                if (point===".") {
+                    mt +="10"
+                } if (point=== "-") {
+                    mt += "11"
+                } 
+            } 
+            while (mt.length<10) {
+                let doubleZero = "00";
+                mt = doubleZero.concat(mt);
+            }  
+           
+        return MORSE_TABLE_ONE_ZERO[mt] = MORSE_TABLE[key];
+           
+       });  
+    }
+console.log(MORSE_TABLE_ONE_ZERO);
+
+    // console.log (expr.split(""));
+    // for (i=9; i<expr.length; i+=10 ){
+        // console.log (expr[i], (i));
+    // }
+    
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('00000011110000000010'));
