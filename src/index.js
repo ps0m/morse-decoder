608c2e009b3200bb605e4ff362e9dea0be68e8ac
ps0m <ps0m@tut.by>
2022-02-06 01:47:32 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -37,39 +37,42 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    let word = '';
-    const MORSE_TABLE_ONE_ZERO = {};
-    for (key in MORSE_TABLE) {
-        let mt='';
-        [key].splice("").map(function (x){
-            for (point of x) {
-                if (point===".") {
-                    mt +="10"
-                } if (point=== "-") {
-                    mt += "11"
-                } 
+const MORSE_TABLE_ONE_ZERO = {};
+for (key in MORSE_TABLE) {
+    let mt='';
+    [key].splice("").map(function (x){
+        for (point of x) {
+            if (point===".") {
+                mt +="10"
+            } if (point=== "-") {
+                mt += "11"
             } 
-            while (mt.length<10) {
-                let doubleZero = "00";
-                mt = doubleZero.concat(mt);
-            }  
-           
-        return MORSE_TABLE_ONE_ZERO[mt] = MORSE_TABLE[key];
-           
-       });  
-    }
+        } 
+        while (mt.length<10) {
+            let doubleZero = "00";
+            mt = doubleZero.concat(mt);
+        }  
+       
+    return MORSE_TABLE_ONE_ZERO[mt] = MORSE_TABLE[key];
+   });  
+}
+MORSE_TABLE_ONE_ZERO["**********"] = " ";
+
 console.log(MORSE_TABLE_ONE_ZERO);
 
-    // console.log (expr.split(""));
-    // for (i=9; i<expr.length; i+=10 ){
-        // console.log (expr[i], (i));
-    // }
-    
+function decode(expr) {
+    let word = '';
+    for (i=0; i<expr.length; i+=10 ){
+        if (i === " ") {
+            word += " ";
+        }
+        word += MORSE_TABLE_ONE_ZERO[expr.slice(i, i+10)]; 
+    }
+    return word;    
 }
 
 module.exports = {
     decode
 }
 
-console.log(decode('00000011110000000010'));
+// console.log(decode('00000011110000000010'));
